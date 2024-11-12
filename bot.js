fa57dc4e77874fdb85522a187bee98de6065d648
Njay2trappy <tmerchant433@gmail.com>
2024-11-12 14:04:08 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,164 @@
+[
+  {
+    "user_id": 1714624187,
+    "deposit_amount": 0.01842588,
+    "deposit_address": "0xD192B8DBFD76864aB94864DB8154a43c2E4A712E",
+    "private_key": "0xc8cf601b1b68b4f61324d468c26da9d9b37399da06c8d535ab3c687ee59e50c0",
+    "deposit_status": "Deposit Success",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T03:52:23.232Z"
+  },
+  {
+    "user_id": 1714624187,
+    "deposit_amount": 0.1150095,
+    "deposit_address": "0xA738Fdcef01588b8da3650236FE79D3d785013db",
+    "private_key": "0x921d94b8a946c01766f5f64d3c0a5b62b6cc1e72be6faf03c3eb18e9c38454e7",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T04:02:31.755Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.007708899999999999,
+    "deposit_address": "0x64c69A4626494a7C1CB09BfEa7D06ccD01A252ef",
+    "private_key": "0x4c43c6c1922b27d31633314d9aa1a7814c7c1fd78f5eb81128b8977d08668c44",
+    "deposit_status": "Withdrawn",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T09:41:14.245Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.015417799999999999,
+    "deposit_address": "0xF6a5164Ef0598218956422f09e6ea1D656c98526",
+    "private_key": "0x41a6e13a17b4abacbdca494f7d01c2824633435f79637590612938ae74bce6b0",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T09:42:47.805Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.007708899999999999,
+    "deposit_address": "0xfb22c8Dc21763651A699401d70740D08011e55Df",
+    "private_key": "0xb760fd8f0ae58c92a4c515e076d9ef9c9b41b49b299c2d7e78877ae4e9a6cfde",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T09:42:59.002Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00317102,
+    "deposit_address": "0xaaf1475e900F45BDc34f0b5c7782f5E4818b06bA",
+    "private_key": "0xaa77d1cacbd2d95ba38b38e13c087f61ae55e299afc062ce17a333ff28dd435b",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T11:47:14.461Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.015941900000000002,
+    "deposit_address": "0xF490BF12d8cb5dc138e15f89717682545b98C00e",
+    "private_key": "0xb69e80079e32291cf90ebc1df79c629ffcaa824fed938d37c8103f72596844e9",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T11:56:45.686Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00478257,
+    "deposit_address": "0x4fA88f63BABA3D2F048dC1599Aa548CD7b2DECAF",
+    "private_key": "0x734f4c44962252e8561736cf0efab712aa5124062ad05e9a32e945b27e4608f3",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T11:59:43.919Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0079431,
+    "deposit_address": "0xE2582AcF7202986EacC63c9d5e5ddf871dE3d92c",
+    "private_key": "0xe39586e861bf433d22f6f8fa4ccf6b5a188304b33f6c6fe9f105b612aa092da5",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:16:08.226Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0158862,
+    "deposit_address": "0x41B4bBb28874Dd47D8475588E0951FEbeCd80D62",
+    "private_key": "0x33219db8d5a77c0291cfb44b3af842b9e4611546bd6432c0b3511f6c59bb7610",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:20:44.472Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0158978,
+    "deposit_address": "0x0bFD27f079c8FB878f658844DbDE31209e41463e",
+    "private_key": "0x5407aff55c80bf784803340be84cbd2312286711cc58bc033f8c146b0a2f1321",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:22:29.200Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0158978,
+    "deposit_address": "0xD39619b8f33d9647E2EeC0cB4Ae823D939DAc0dd",
+    "private_key": "0xd8491f88ba061cd0199717dfee641768f1227007206b217203bc4024c065e5db",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:22:29.214Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0047880299999999995,
+    "deposit_address": "0xe441f5B16E0Af942E49bca3aF97DCF45dC050a2F",
+    "private_key": "0xadc026ac7503effa273b8b05af1245dcd802d001f74547c2c87fa94907fdb2e1",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:30:19.755Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00479436,
+    "deposit_address": "0xbEa335e1Cf68de96cF21252ED3ed8e04D4d7D334",
+    "private_key": "0xc4f394f25fc248c2e236d46aeec5efb0c766a1c598aa6ed3bdd445a8fa8946e9",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:40:05.783Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00479436,
+    "deposit_address": "0x3157cE236EB7365b7c54878976dc14cB92FbB94b",
+    "private_key": "0x6925b7017511ddd2e30822e14a29f91260e2716a3306d7cd1b4cb3342bb57f99",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:41:41.837Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.0009227700000000001,
+    "deposit_address": "0x8358Ff9F06e41f449C1A538233018df968209849",
+    "private_key": "0xadd9e6175891472183fc7f1f72e37e7767a63aa339be8b34c6ab4f8990d547d5",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:52:16.384Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00483117,
+    "deposit_address": "0x48f6944f356406352684182db6139c5Be57DaC9e",
+    "private_key": "0x3b45c0328ca86c647363da3860600d0e91a9ef23d5c3c26ab7b0832c19c9421e",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T12:56:08.352Z"
+  },
+  {
+    "user_id": 711628241,
+    "deposit_amount": 0.00482286,
+    "deposit_address": "0x7180Cae01266f59385372477115E5F26Caa185b5",
+    "private_key": "0xc514f6eaec8e90a82e3e6aadcd287fa4fb6dd151f5be561c580d4105a3358fe2",
+    "deposit_status": "Pending",
+    "withdrawal_status": "Not Withdrawn",
+    "created_at": "2024-11-12T13:02:04.909Z"
+  }
+]
